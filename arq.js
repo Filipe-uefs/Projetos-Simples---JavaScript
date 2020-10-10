669d10ae621792682a70101a2e99c31717145b2c
Filipe-uefs <lipe_0109@hotmail.com>
2020-10-09 21:53:56 -0300

    melhorando nomes das variaveis e functions

--- arq.js
+++ arq.js
@@ -1,26 +1,31 @@
 //Função que calcula o valor de binário para decimal 
-        function myFunction(){
+        function CalcDecimal(){
             var binario = document.getElementById("binario").value;
             var numeroFinal = 0;
+            var confirme = true
             var expo = binario.length-1;
             for(var x=0;x<binario.length;x++){
-                console.log(binario[x]);
                 if (binario[x]=="1"){
                     numeroFinal = numeroFinal + (2**expo);
+                } else if (binario[x]!="0"){
+                    var resultado = document.getElementById("resulBinario");
+                    resultado.innerText = "Número digitado é inválido";
+                    confirme = false
                 }
                 expo--;
+            } // end for 
+            if(confirme){
+                var resultado = document.getElementById("resulBinario");
+                resultado.innerText = "Resultado: " + numeroFinal;
             }
-            var resultado = document.getElementById("resulBinario");
-            resultado.innerText = "Resultado: " + numeroFinal;
-        }
+        } // end CalcDecimal()
         // Função que calcula de decimal para binário
-        function myFunction2(){
+        function CalcBinario(){
             var decimal = document.getElementById("decimal").value;
             var cont = 0;
             decimal = parseInt(decimal);
             var resultado = ""
             for(var x=0;2**x<parseInt(decimal);x++);
-            console.log(x);
             for(var i=0;i<=x;i++){
                 if(decimal-(2**(x-i))>=0){
                     console.log(i);
